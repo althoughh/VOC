09b8085451855783775ee0d7695d7de4ab5a6ed5
althoughh <95028240+althoughh@users.noreply.github.com>
2023-09-26 17:10:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,45 @@
-
 import streamlit as st
 import pandas as pd
-import collections
 
-# The URL of the raw data file on GitHub
-url = "https://raw.githubusercontent.com/althoughh/VOC/main/data.csv"
+# Read CSV files from URLs
+industry_url = 'https://github.com/althoughh/VOC/blob/main/industry.csv'
+individual_url = 'https://github.com/althoughh/VOC/blob/main/individual.csv'
+jobs_url = 'https://github.com/althoughh/VOC/blob/main/jobs.csv'
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv(url)
+industry_df = pd.read_csv(industry_url)
+individual_df = pd.read_csv(individual_url)
+jobs_df = pd.read_csv(jobs_url)
 
+# Streamlit app starts here
+st.title("Industry Information Dashboard")
 
-def main():
-    st.title("Product Analysis Dashboard")
+# Filters
+st.sidebar.title("Filters")
 
-    st.header("Choose a question to answer")
+selected_industry = st.sidebar.multiselect("Select Industry", industry_df['Industry Name'].unique())
+selected_role = st.sidebar.multiselect("Select Role", individual_df['Role Name'].unique())
+selected_job = st.sidebar.multiselect("Select Jobs to be Done", jobs_df['Job Name'].unique())
 
-    question_option = st.selectbox(
-        "",
-        (
-            "Most common phrases for Users with a [sentiment] experience when talking about [category]",
-            "Most common [categories] amongst [industry]",
-            "Most common phrases for people with a [score] rating",
-            "For [product/slug], [sentiment] reviews contained the following phrases",
-            "Negative reviews common phrases of [product/slug] compared to [product/slug]",
-            "For [product/slug] the following categories were most prominent when the experience was [sentiment]"
-        ),
-    )
-
-    if question_option == "Most common phrases for Users with a [sentiment] experience when talking about [category]":
-        sentiment_option = st.selectbox("Choose sentiment", df['sentiment'].unique())
-        category_option = st.selectbox("Choose category", df['category'].unique())
-        # Perform your analysis here based on the selected sentiment and category
-
-    elif question_option == "Most common [categories] amongst [industry]":
-        industry_option = st.selectbox("Choose industry", df['industry'].unique())
-        # Perform your analysis here based on the selected industry
-
-    elif question_option == "Most common phrases for people with a [score] rating":
-        score_option = st.selectbox("Choose score", df['score'].unique())
-        # Perform your analysis here based on the selected score
-        filtered_df = df[df['score'] == score_option]
-        common_phrases = collections.Counter(" ".join(filtered_df["review_processed"]).split()).most_common(5)
-        st.write("Most common phrases:", common_phrases)
-
-    # Add other questions and their corresponding dropdowns and analysis
+# Button to confirm selection and show tables
+if st.sidebar.button('Show Data'):
     
-    st.write("Your selected options will be used for analysis here.")
+    # Filter and display tables only if filters are selected
+    if selected_industry or selected_role or selected_job:
+        st.header("Filtered Data")
 
-if __name__ == "__main__":
-    main()
+        if selected_industry:
+            st.subheader("Industry")
+            st.write(industry_df[industry_df['Industry Name'].isin(selected_industry)].drop(columns=['Industry ID']))
+
+        if selected_role:
+            st.subheader("Individual")
+            filtered_individual_df = individual_df[individual_df['Role Name'].isin(selected_role)]
+            st.write(filtered_individual_df.drop(columns=['Individual ID', 'Industry ID']))
+
+        if selected_job:
+            st.subheader("Jobs to be Done")
+            filtered_jobs_df = jobs_df[jobs_df['Job Name'].isin(selected_job)]
+            st.write(filtered_jobs_df.drop(columns=['Job ID', 'Industry ID', 'Individual ID']))
+
+    else:
+        st.write("No filters selected. Please choose options from the sidebar.")
